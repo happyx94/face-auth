a3fb0ed65c47910f6101136d95be0fb3ed26be37
ericlin94 <eric50818244@gmail.com>
2018-07-08 12:27:12 -0400

    Login and Transaction page

--- index.js
+++ index.js
@@ -3,60 +3,66 @@ var request 	= require("request");
 var mongoose 	= require("mongoose");
 var app			= express();
 var	User        = require("./models/user");
+var bodyparser=require("body-parser");
+
+
 
 var Rehive = require("rehive");
-var config = {apiVersion: 3, apiToken: 'd73e226d213fdb761533666a4ca9e4201cbb06a913e5b9449f79e668fae3fce9'};
-const r = new Rehive(config);
 
-const IP = '10.123.14.21';
+var config = {apiVersion: 3, apiToken: 'd73e226d213fdb761533666a4ca9e4201cbb06a913e5b9449f79e668fae3fce9'}; 
+
+var r = new Rehive(config);
+
+const IP = 'localhost';
 const PORT = 8080;
 const APP_TITLE = 'Face Auth';
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://' + IP + '/face-auth');
+//mongoose.connect('mongodb:');
+app.use(bodyparser.urlencoded({extended:true})); 
 
-	
+
 /* Routes */
 app.get('/', function(req, res) 
 {
-	//login
-
-	r.auth.login({
-	    user: "eric50818244@gmail.com",
-	    company: "ericlin94",
-	    password: "eric40418204"
-	}).then(function(user){
-    	
-	},function(err){
-    
-	})
-	
-	//transaction
-
-	r.transactions.createTransfer(
-	{
-	    amount: 50*100,
-	    recipient: "LP547887@gmail.com",
-	    currency: "USD"
-
-	}).then(function(res){
-	    console.log(res)
-	},function(err){
-	    console.log(err)
-	})
-	
     res.redirect('/index');
 });
+
+//login routes
+app.get('/login',function(req,res){
+	res.render('login')
+})
+app.post('/login',function(req,res){
+	res.render('options')
+})
+//transaction Root
+app.get('/transaction',function(req,res){
+	res.render('transaction')
+})
+app.post('/transaction',function(req,res){
+	//if face-auth success
+	console.log(req.body.Amount)
+	console.log(req.body.Recipient)
+	console.log(req.body.CurrencyType)
+	r.transactions.createTransfer(
+	{
+	    amount: req.body.Amount*100,
+	    recipient: req.body.Reciepiant,
+	    currency: req.body.CurrencyType
+
+	}).then(function(res2){
+	    console.log(res2)
+	    res.send('success')
+	},function(err){
+	    console.log(err)
+	    res.send(err)
+	})
+})
 
 app.get('/index', function(req, res) 
 {
     res.render('index', { title: APP_TITLE });
-});
-
-app.get('/login', function(req, res) 
-{
-    res.render('login', { title: APP_TITLE });
 });
 
 app.get('/mockuser/:name', function(req, res)
